e2732dd7574b4f237d693bbbe8b825d89c222b74
Hummingbird-12 <goodboyih96@gmail.com>
2018-04-11 09:17:54 +0900

    BIRD modelling

--- 3.1.2DObjects_GLSL/3.1.2DObjects_GLSL.cpp
+++ 3.1.2DObjects_GLSL/3.1.2DObjects_GLSL.cpp
@@ -857,6 +857,73 @@ void draw_sword() {
 	glBindVertexArray(0);
 }
 
+// MODEL : BIRD
+
+#define BIRD_BODY 0
+#define BIRD_BEAK 1
+#define BIRD_EYE 2
+#define BIRD_PUPIL 3
+
+GLfloat bird_body[4][2] = { { -15.0, -15.0 }, { -15.0, 15.0 }, { 15.0, 15.0 }, {15.0, -15.0} };
+GLfloat bird_beak[3][2] = { { 15.0, 7.0 }, { 15.0, -7.0 }, { 30.0, 0.0 } };
+GLfloat bird_eye[1][2] = { 7.0, 7.0 };
+GLfloat bird_pupil[1][2] = { 8.0, 7.0 };
+
+GLfloat bird_color[4][3] = {
+	{ 255.0f / 255.0f, 0 / 255.0f, 0 / 255.0f },
+	{ 255.0f / 255.0f, 200.0f / 255.0f, 0 / 255.0f },
+	{ 255.0f / 255.0f, 255.0f / 255.0f, 255.0f / 255.0f },
+	{ 0.0f / 255.0f, 0.0f / 255.0f, 0.0f / 255.0f }
+};
+
+GLuint VBO_bird, VAO_bird;
+void prepare_bird() {
+	GLsizeiptr buffer_size = sizeof(bird_body) + sizeof(bird_beak) + sizeof(bird_eye) + sizeof(bird_pupil);
+
+	// Initialize vertex buffer object.
+	glGenBuffers(1, &VBO_bird);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_bird);
+	glBufferData(GL_ARRAY_BUFFER, buffer_size, NULL, GL_STATIC_DRAW); // allocate buffer object memory
+
+	glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(bird_body), bird_body);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(bird_body), sizeof(bird_beak), bird_beak);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(bird_body) + sizeof(bird_beak), sizeof(bird_eye), bird_eye);
+	glBufferSubData(GL_ARRAY_BUFFER, sizeof(bird_body) + sizeof(bird_beak) + sizeof(bird_eye), sizeof(bird_pupil), bird_pupil);
+
+	// Initialize vertex array object.
+	glGenVertexArrays(1, &VAO_bird);
+	glBindVertexArray(VAO_bird);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO_bird);
+	glVertexAttribPointer(LOC_VERTEX, 2, GL_FLOAT, GL_FALSE, 0, BUFFER_OFFSET(0));
+
+	glEnableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindVertexArray(0);
+}
+
+void draw_bird() {
+	glBindVertexArray(VAO_bird);
+
+	glUniform3fv(loc_primitive_color, 1, bird_color[BIRD_BODY]);
+	glDrawArrays(GL_TRIANGLE_FAN, 0, 4);
+
+	glUniform3fv(loc_primitive_color, 1, bird_color[BIRD_BEAK]);
+	glDrawArrays(GL_TRIANGLE_FAN, 4, 3);
+
+	glUniform3fv(loc_primitive_color, 1, bird_color[BIRD_EYE]);
+	glPointSize(10.0);
+	glDrawArrays(GL_POINTS, 7, 1);
+	glPointSize(1.0);
+
+	glUniform3fv(loc_primitive_color, 1, bird_color[BIRD_PUPIL]);
+	glPointSize(4.0);
+	glDrawArrays(GL_POINTS, 8, 1);
+	glPointSize(1.0);
+
+	glBindVertexArray(0);
+}
 
 // display function
 
@@ -906,6 +973,11 @@ void display(void) {
 	ModelViewProjectionMatrix = ViewProjectionMatrix * ModelMatrix;
 	glUniformMatrix4fv(loc_ModelViewProjectionMatrix, 1, GL_FALSE, &ModelViewProjectionMatrix[0][0]); 
 	draw_car2();
+
+	ModelMatrix = glm::mat4(1.0f);
+	ModelViewProjectionMatrix = ViewProjectionMatrix * ModelMatrix;
+	glUniformMatrix4fv(loc_ModelViewProjectionMatrix, 1, GL_FALSE, &ModelViewProjectionMatrix[0][0]);
+	draw_bird();
 
 	glFlush();	
 }   
@@ -981,6 +1053,7 @@ void prepare_scene(void) {
 	prepare_car();
 	prepare_cocktail();
 	prepare_car2();
+	prepare_bird();
 }
 
 void initialize_renderer(void) {
